005c5489af6b418bcf880d8992d35650a3d3b01e
BryanPlaces <eduardo.places@gmail.com>
2018-08-28 11:27:05 +0100

    modificacion prod.env y dev.env

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,5 +4,6 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  ROOT_API: '"http://localhost:4000"'
+  ROOT_API: '"http://localhost:4000"',
+  PROD_URL: '"http://billycost.hopto.org:4000"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  ROOT_API: '"http://localhost:4000"'
+  ROOT_API: '"http://localhost:4000"',
+  PROD_URL: '"http://billycost.hopto.org:4000"'
 }
